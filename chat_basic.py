157e00e925547cb406d23b3620684316ff7d4f6d
Lorenze Jay <lorenzejaytech@gmail.com>
2024-05-11 21:24:53 -0700

    better server

--- chat_basic.py
+++ chat_basic.py
@@ -1,9 +1,20 @@
-from interpreter import interpreter
+from interpreter import OpenInterpreter
+interpreter = OpenInterpreter(import_computer_api=True)
 
-interpreter.llm.model='llama3-70b-8192'
-interpreter.llm.api_key= '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'
-interpreter.llm.api_base='https://api.groq.com/openai/v1'
+# interpreter.llm.model='llama3-70b-8192'
+interpreter.llm.model='gpt-4-turbo'
+
+# interpreter.llm.api_key= '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'
+# interpreter.llm.api_base='https://api.groq.com/openai/v1'
 interpreter.auto_run=True
 interpreter.verbose=True
+interpreter.max=True
+# interpreter.custom_instructions=''
+interpreter.os=True
+# interpreter.computer
 
-interpreter.chat('open youtube on safari')
+# interpreter.chat('open slack app and send a message saying "hello world from our automation" to Federico') 
+# interpreter.chat('search for ai events on cerebral valley and add all the future events you see to the numbers app save the sheet as upcoming events.') 
+interpreter.chat('open the Obsidian app and create a new note with command + n, call it hello world from shack15, and write a blurb about a hackathon and winning it') 
+# does not work
+# interpreter.chat('open twitter on Google Chrome and type a tweet saying hello from @shack15.') 
--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
-from interpreter import interpreter
 from pydantic import BaseModel
+from interpreter import OpenInterpreter
+interpreter = OpenInterpreter(import_computer_api=True)
 
 app = FastAPI()
 
@@ -12,9 +13,11 @@ class ChatMessage(BaseModel):
 async def chat_endpoint(chat: ChatMessage):
     print('data',chat)
     interpreter.auto_run = True
-    interpreter.llm.model='llama3-70b-8192'
-    interpreter.llm.api_key= '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'
-    interpreter.llm.api_base='https://api.groq.com/openai/v1'
+    interpreter.llm.model='gpt-4-turbo'
+    # interpreter.llm.model='llama3-70b-8192'
+    # interpreter.llm.api_key= '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'
+    # interpreter.llm.api_base='https://api.groq.com/openai/v1'
+    interpreter.custom_instructions='My work apps are: ["Arc", "Google Chrome", "Obsidian", "Xcode"], sometimes you need to open or close them.'
     interpreter.auto_run=True
     interpreter.verbose=True
 
@@ -22,22 +25,19 @@ async def chat_endpoint(chat: ChatMessage):
     return {
       'messages': messages
     }
+
 @app.post("/api/follow-up")
 async def chat_endpoint_2(chat: ChatMessage):
     interpreter.auto_run = True
     interpreter.llm.model='llama3-70b-8192'
     interpreter.llm.api_key= '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'
     interpreter.llm.api_base='https://api.groq.com/openai/v1'
+    interpreter.custom_instructions='My work apps are: ["Arc", "Google Chrome", "Obsidian", "Xcode"], sometimes you need to open or close them.'
     interpreter.auto_run=True
     print('interpreter.messages', interpreter.messages)
     result = interpreter.chat(chat.command)
     print('result', result)
     
-    return {
-      "messages": messages
-    }
-
-
 @app.get("/history")
 def history_endpoint():
     return interpreter.messages
